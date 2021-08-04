30ce6bd000bb35510553d214692ea7b75fcda24b
Antony <vontonnie@gmail.com>
2021-08-04 20:48:19 +0300

    Add getAnimSpec()

--- app/src/main/java/com/uxstate/composeanimation/MainActivity.kt
+++ app/src/main/java/com/uxstate/composeanimation/MainActivity.kt
@@ -3,10 +3,14 @@ package com.uxstate.composeanimation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.core.AnimationSpec
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.uxstate.composeanimation.ui.theme.ComposeAnimationTheme
 
@@ -24,6 +28,31 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+@Composable
+fun AnimationLayout() {
+
+    var state by remember { mutableStateOf(false) }
+    var animSpec by remember { mutableStateOf("snap") }
+
+
+    val startColor = Color.Blue
+    val endColor = Color.Green
+
+    val backgroundColor by animateColorAsState(
+        targetValue = if (state) endColor else startColor)
+    animSpec = getAnimSpec(animSpec)
+
+
+
+    Column(){}
+
+
+}
+
+fun getAnimSpec(animSpec: String): AnimationSpec<Color> {
+
+}
 
 
 
