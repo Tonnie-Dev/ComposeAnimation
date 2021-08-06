e540e5402e9a21edc6d85b813abd6dcfd4adcfe9
Antony <vontonnie@gmail.com>
2021-08-06 08:53:57 +0300

    Include the Blog Link

--- app/src/main/java/com/uxstate/composeanimation/MainActivity.kt
+++ app/src/main/java/com/uxstate/composeanimation/MainActivity.kt
@@ -5,11 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.animation.core.AnimationSpec
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.uxstate.composeanimation.ui.theme.ComposeAnimationTheme
@@ -40,19 +44,49 @@ fun AnimationLayout() {
     val endColor = Color.Green
 
     val backgroundColor by animateColorAsState(
-        targetValue = if (state) endColor else startColor)
+        targetValue = if (state) endColor else startColor
+    )
     animSpec = getAnimSpec(animSpec)
 
 
 
-    Column(){}
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(backgroundColor), verticalArrangement =
+        Arrangement.Center
+    ) {
+
+
+
+    }
 
 
 }
 
-fun getAnimSpec(animSpec: String): AnimationSpec<Color> {
 
+@Composable
+fun AnimationButton(state: Boolean, onValueChanged:(Boolean, String)-> Unit){
+
+
+
+}
+
+fun getAnimSpec(spec: String): AnimationSpec<Color> {
+
+
+    return when(spec){
+
+
+
+       animationSpec.SPRING.value -> {
+
+
+
+        }
+    }
 }
 
 
 
+/*https://medium.com/frndapp/a-quick-guide-to-the-animate-asstate-api-in-jetpack-compose-animation-ab1ba3b6379f*/
